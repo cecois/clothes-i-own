f5bc4adbd3837278cf0b83ebacea2fc1cd36c024
cecois <cecmcgee@gmail.com>
2017-04-19 09:32:47 -0400

    img and js markdown gen working

--- processing/img.js
+++ processing/img.js
@@ -5,22 +5,23 @@ var FS = require('fs')
 var rawdir = "/Users/ccmiller/Desktop/pho/clothes/"
 
 FS.readdir(rawdir, (err, files) => {
-  files.forEach(file => {
   i=01;
-  if(file.indexOf("IMG_")==0){
+  files.forEach(file => {
+  if(file.indexOf(".png")>=0){
   console.log("processing file: "+file);
 
-GM(rawdir+file)
+GM(rawdir+file).type('Optimize')
 // .autoOrient()
  // .colorize(80, 0, 30)
- .contrast(1.5)
+ .geometry(472,666)
+ .contrast(-2)
  // .colors(64)
  // .gamma(5, 5, 5)
-.write("/Users/ccmiller/git/clothes-i-own/assets/img/project/imgk-"+file, function (err) {
+.write("../assets/img/garment/imgk-"+i+"-"+file, function (err) {
   if (err){console.log(err)}
 })
 
   }
-
+i++;
   });
 })
--- processing/js.js
+++ processing/js.js
@@ -1,28 +1,46 @@
 var FS = require('fs')
 ,MARKDOWN = require( "markdown" ).markdown
+,__ = require("underscore")
+,ANNO = require("./anno.json")
 ;
 
-FS.readdir('/Users/ccmiller/git/clothes-i-own/assets/img/project', (err, files) => {
+FS.readdir('../assets/img/garment', (err, files) => {
   files.forEach(file => {
-  i=01;
-  if(file.indexOf("IMG")==0){
-  	var im = file.split("_")[1].split('.JPG')[0]
+  	if(file.indexOf("imgk")==0){
 
-var md = '---\nlayout: project\ntitle:  "YOUR PROJECT NAME"\ndate:  2017-04-15 16:54:46\nauthor: C.C. Miller\n'
-md+='categories:\n- project\nimg: '+file+'\nthumb: '
-md+=file+"-thumb.jpg"
-md+='\ntagged: Flat,UI,Development\nclient: Wonder Corp.\n'
-md+='website: http://blacktie.co\n---\n####YOUR PROJECT NAME\nLorem Ipsum\n####Our Proposal\nLorem Ipsum.'
+  	  	var id = file.split("_")[1].split(".png")[0]
 
-  	FS.writeFile("/Users/ccmiller/git/clothes-i-own/_projects/2017-04-15-"+im+".markdown", md, function(err) {
-    if(err) {
-        return console.log(err);
-    }
+  	  	var anno = __.findWhere(ANNO,{id:id})
 
-    console.log("2017-04-"+i+"-"+im+" saved");
-    i++;
-}); 
-  }
-
+var nom = (typeof anno !== 'undefined')?anno.name:"no name";
+var origin = (typeof anno !== 'undefined')?anno.origin:"no origin";
+var tags = (typeof anno !== 'undefined')?anno.tags:"no tags";
+  	  
+  	  i=01;
+  	  if(file.indexOf(".png")>=0){
+  	  	var im = file.split("_")[1].split('.png')[0]
+  	
+  	var md = '---\nlayout: garment'
+  	md+='\ntitle:  "'+nom+'"\ndate:  2017-04-15 16:54:46\nauthor: C.C. Miller'
+  	md+='\ncategories:\n- clothes'
+  	md+='\nimg: '+file
+  	// +file+'\nthumb: '
+  	// md+=file+"-thumb.jpg"
+    md+='\norigin: '+origin
+  	md+='\ntagged: '+tags
+    // \nclient: Wonder Corp.'
+  	md+='\nwebsite: http://clothes-lbones.rhcloud.com\n---'
+  	md+='\n####'+nom+'\nLorem Ipsum\n####Our Proposal\nLorem Ipsum.'
+  	
+  	  	FS.writeFile("../_clothes/2017-04-15-"+im+".markdown", md, function(err) {
+  	    if(err) {
+  	        return console.log(err);
+  	    }
+  	
+  	    // console.log("2017-04-"+i+"-"+im+" saved");
+  	    i++;
+  	}); 
+  	  }
+  	} //if imgk
   });
 })
